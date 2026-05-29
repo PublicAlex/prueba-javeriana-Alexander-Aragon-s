4e84f17845cdac3abd2aee2e5630b54e7a6a2e76
Alexander <aragonantar@gmail.com>
2026-05-29 10:20:24 -0500

    improve docker volumes

--- backend/biblioteca_api/settings.py
+++ backend/biblioteca_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-=w8xyv6_0)p-fftuipk9ew2-ns8qw^t0b_%&2*b^yrky=!b&nl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
@@ -77,7 +78,7 @@ WSGI_APPLICATION = "biblioteca_api.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "data" / "db.sqlite3",
     }
 }
 
